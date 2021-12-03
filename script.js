d62260bbfa20131e73b87a5a404ef344e797c782
Usman Ali <91782435+Uxmanmirza@users.noreply.github.com>
2021-12-03 17:32:24 +0500

    complete

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+let myCount = 0;
+const result = document.getElementById("result");
+const positiveBtn = document.getElementById("positiveBtn");
+const negativeBtn = document.getElementById("negativeBtn");
+positiveBtn.onclick = function() {
+    //increment method in counter
+    myCount += 1;
+    result.innerText = myCount;
+};
+negativeBtn.onclick = function() {
+    //decrrement method in counter
+    myCount -= 1;
+    result.innerText = myCount;
+};
